d1a2c88373b0810147dd9f409c0a3c77f1c2ea19
Jesse Winton <jrandallwinton@gmail.com>
2023-04-06 14:13:53 -0400

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,11 @@ const Index = () => {
   return (
     <div className="flex flex-col gap-2 pb-4">
       <div className="relative z-50">
+        <div className="py-20">
+          <h1 className="font-serif text-4xl font-light">
+            Learn the skills you need to succeed in the performing arts.
+          </h1>
+        </div>
         <div
           className="space-y-4 font-light"
           dangerouslySetInnerHTML={{ __html: data.body.html }}
